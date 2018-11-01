d35e040667f6bbd8865c18c484323822da5389e9
hh <robert@hammelrath.com>
2018-11-01 13:32:54 +0100

    ws2812.py: Streamline the code
    - the temporary bytearry bits is not used, but created on the fly
    in the call to rmt.pulses_send
    - assignment ot the durations is made by a ternary expression
    - drop the second sleep_us

--- ws2812.py
+++ ws2812.py
@@ -1,5 +1,5 @@
 #
-# class for driving WS2812 Neopixel device on a Pycom 
+# class for driving WS2812 Neopixel device on a Pycom
 # module using the RMT interface
 #
 
@@ -7,28 +7,19 @@ class WS2812:
     def __init__(self, pin, channel=3):
         from machine import RMT
         self.rmt = RMT(channel=channel, gpio=pin, tx_idle_level=0)
-        
+
     def show(self, data):
         from time import sleep_us
-        no_pixel = len(data)
-        bits = bytearray(no_pixel * 24 * 2)
-        duration =  bytearray(no_pixel * 24 * 2)
+        duration =  bytearray(len(data) * 24 * 2)
         index = 0
         for pixel in data:
             for byte in pixel:
                 mask = 0x80
                 while mask:
-                    bits[index] = 1
-                    bits[index+1] = 0
-                    if byte & mask:
-                        duration[index] = 8
-                        duration[index+1] = 4
-                    else:
-                        duration[index] = 4
-                        duration[index+1] = 8
+                    duration[index:index+2] = \
+                        b'\x08\x04' if byte & mask else b'\x04\x08'
                     index += 2
                     mask >>= 1
         sleep_us(60) # wait for the reset time
-        self.rmt.pulses_send(tuple(duration), tuple(bits))
-        sleep_us(60) # wait for the reset time
+        self.rmt.pulses_send(tuple(duration), (1,0) * (len(data) * 24))
 
