ef1e6bfc0fe147d0c3ab8770c2517fb4df7e4034
hh <robert@hammelrath.com>
2018-12-31 20:45:37 +0100

    WS2812.py: make code PEP8 compliant

--- ws2812.py
+++ ws2812.py
@@ -3,6 +3,7 @@
 # module using the RMT interface
 #
 
+
 class WS2812:
     def __init__(self, pin, channel=3):
         from machine import RMT
@@ -10,7 +11,7 @@ class WS2812:
 
     def show(self, data):
         from time import sleep_us
-        duration =  bytearray(len(data) * 24 * 2)
+        duration = bytearray(len(data) * 24 * 2)
         index = 0
         for pixel in data:
             for byte in pixel:
@@ -20,6 +21,5 @@ class WS2812:
                         b'\x08\x04' if byte & mask else b'\x04\x08'
                     index += 2
                     mask >>= 1
-        sleep_us(60) # wait for the reset time
-        self.rmt.pulses_send(tuple(duration), (1,0) * (len(data) * 24))
-
+        sleep_us(60)  # wait for the reset time
+        self.rmt.pulses_send(tuple(duration), (1, 0) * (len(data) * 24))
